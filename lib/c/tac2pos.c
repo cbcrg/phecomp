e01b12fafe45455a52163ab54db486ad8d9deb32
José Antonio Espinosa <kadomu@gmail.com>
2011-06-17 10:13:12 +0000

    readAct.c has been changed to tac2pos.c which has an act2pos.pl-like output

--- lib/c/tac2pos.c
+++ lib/c/tac2pos.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 /* Reading tac files */
 
@@ -30,7 +31,7 @@ typedef struct
 	char endName[1];
 	char name[35];
 	double creationDate;
-	double modificationDate;
+	double modDate;
 	int reserved; //not used
 	int trajectories;
 
@@ -103,15 +104,14 @@ typedef struct
 
 //Function declaration
 //functions should be inside decl.c
-void fileProcessing (FILE *fd);
+void fileProcessing (FILE *fd, char * name);
 chunk returnChunkHeader (FILE *fd);
 int countBytes (FILE *fd);
 void asessTag (char fileTag[], char Tag[]);
 void readCoordinates (int size, FILE *fd, float hCal, float vCal);
 int excelTime2EPOCH (double dateTrack);
-
-
-
+char * returnTimeString (double EPOCHseconds, int n);
+void printTrackHeaders (FILE *fd, int tracks, char * fileName);
 int main (int argc, char *argv[])
 //int main ()
 {
@@ -124,7 +124,10 @@ int main (int argc, char *argv[])
   {
 	 FILE *fd;
 
-	 fd = fopen (argv[fileCtr], "rb");
+	 char * fileName;
+	 fileName = argv[fileCtr];
+
+	 fd = fopen (fileName, "rb");
 
 	 if ( fd == NULL )
 	 {
@@ -132,8 +135,8 @@ int main (int argc, char *argv[])
 	   exit (1);
      }
 
-	 fileProcessing (fd);
-	 printf ("file is %s\n\n" , argv[fileCtr]);
+	 fileProcessing (fd, fileName); //fileName only for printing purpose
+	 printf ("file is %s\n\n" , fileName);
 	 fclose (fd);
   }
 
@@ -142,7 +145,7 @@ int main (int argc, char *argv[])
 
 /*Functions
 */
-void fileProcessing (FILE *fd)
+void fileProcessing (FILE *fd, char * name)
   {
 	  char msg[]="test";
 	  fprintf (stderr, "%s\n", msg);
@@ -151,9 +154,7 @@ void fileProcessing (FILE *fd)
 	  fileHeader = returnChunkHeader (fd);
 
 	  asessTag (fileHeader.id, "RIFF");
-	  //asessTag (fileHeader.type, "PRJ ");
 
-	  //fseek (fd, SEEK_CUR, SEEK_END);
 	  fprintf (stderr, "Annotated file size: %d \n", fileHeader.size);//chivato
 
 	  int count = 0;
@@ -188,13 +189,39 @@ void fileProcessing (FILE *fd)
 	  fprintf (stderr, "Exp remarks endName: %d \n", expRemarks.endName[0]);//chivato
 	  fprintf (stderr, "Exp remarks Name: %s \n", expRemarks.name);//chivato
 
+	  //PRINTING FILE HEADERS
+	  //char name2print[35] = expHeader.name; //if we don't know the size use malloc
+	  //Preparing string variables to be print
+	  char name2print[expHeader.endName[0]];
+	  strncpy (name2print, expHeader.name, expHeader.endName[0]);
+	  name2print [expHeader.endName[0]] = '\0';
+
+	  char * date2printCreation;
+	  char * date2printMod;
+
+	  date2printCreation = returnTimeString (expHeader.creationDate, 80);
+	  date2printMod = returnTimeString (expHeader.modDate, 80);
+
+	  fprintf (stdout, "#h;%s;TRACKING FILE;TRACKING FILE;File Creation Date & Time;%s", name, date2printCreation);
+	  fprintf (stdout, "#h;%s;TRACKING FILE;TRACKING FILE;Last Modification Date & Time;%s", name, date2printMod);
+	  fprintf (stdout, "#h;%s;TRACKING FILE;TRACKING FILE;Code;%s\n", name, name2print);
+	  fprintf (stdout, "#h;%s;TRACKING FILE;TRACKING FILE;Comments;%s\n", name, expRemarks.name);
+	  fprintf (stdout, "#h;%s;HEADER;EHEADER;Ncages;%i\n", name, expHeader.trajectories);//DO THE ASSIGMENT OF THE END STRING
+
+
 	  //TRACK STUFF BEGINS HERE, IT SHOULD BE IN A LOOP OF THE NUMBER O TRACKS (NUMBER OF TRAJECTORIES WILL BE THE COUNTER)
+	  //Printing tracks header first
+
+	  printTrackHeaders (fd, expHeader.trajectories, name);
+
 	  int Ntrack;
+	  //int pdateEPOCH = 0;
 
 	  //for (Ntrack=1; Ntrack <= 12; Ntrack +=1)
 	  for (Ntrack=1; Ntrack <= expHeader.trajectories; Ntrack +=1)
 	  {
-		  fprintf (stderr, "Track being printed +++++++++++++++++++++++++ : %d \n\n\n", Ntrack);
+
+		  fprintf (stderr, "Track being SKIPPED +++++++++++++++++++++++++ : %d \n\n\n", Ntrack);
 
 		  trackInfo trInfo;
 		  fread (&trInfo, sizeof trInfo, 1, fd);
@@ -203,13 +230,15 @@ void fileProcessing (FILE *fd)
 		  fprintf (stderr, "TR size: %d \n", trInfo.size);
 		  fprintf (stderr, "TR listType: %s \n", trInfo.listType);
 
+		  //fseek (fd, trInfo.size, SEEK_CUR);
+
 		  trackFormat trFormat;
 		  fread (&trFormat, sizeof trFormat, 1, fd);
 
-		  fprintf (stderr, "TR format id: %s \n", trFormat.id);
+	/*	  fprintf (stderr, "TR format id: %s \n", trFormat.id);
 		  fprintf (stderr, "TR format id: %s \n", trFormat.id);
 		  fprintf (stderr, "TR format size: %d \n", trFormat.size);
-
+*/
 		  //char * trHeaderBuf;
 		  //trHeaderBuf = (char *) malloc (trFormat.size);
 		  //fread (&trHeaderBuf, sizeof (trHeaderBuf), 1, fd);
@@ -255,21 +284,33 @@ void fileProcessing (FILE *fd)
 		  fprintf (stderr, "TR header endExperimenter: %s \n", experimenter);//del
 		  fprintf (stderr, "TR header trackFile: %d \n", NTrackFile );//del
 
-		  int dateEPOCH = excelTime2EPOCH (dateTrack);
-
-		  //double i = 3.9; #del
-		  //int x;
-		  //x = (int) i;
-		  //printf ("Rounding i: %i\n", x);
-
-
-		  fprintf (stderr, "Creation date EPOCH format: %i\n", dateEPOCH);
-
-		  //dateEPOCHint = (int) dateEPOCH;43165
+//		  int dateEPOCH = excelTime2EPOCH (dateTrack);
+//
+//		  if (dateEPOCH != pdateEPOCH && Ntrack != 1)
+//		  {
+//		    fprintf (stderr, "ERROR: %i time stamps in tracks are not all equal %i\n", dateEPOCH, pdateEPOCH);
+//		    exit (1);
+//		  }
+//
+//
+//		  //double i = 3.9; #del
+//		  //int x;
+//		  //x = (int) i;
+//		  //printf ("Rounding i: %i\n", x);
+//		  pdateEPOCH = dateEPOCH;
+//
+//		  fprintf (stderr, "Creation date EPOCH format: %i\n", dateEPOCH);
+//
+//		  /*Starting time is code in each track so first time is printed in the header and the coincidence with all the
+//		  following tracks time is checked */
+//		  if (Ntrack == 1)
+//		  {
+//		    fprintf (stdout, "#h;%s;HEADER;EHEADER;StartStamp;%i\n", name, dateEPOCH);
+//		  }
 
 
 		  //printf ("Creation date EPOCH format integer: %i\n", dateEPOCHint);
-		  exit(1);
+		  //exit(1);
 		  //int end;
 		  //end = expHeader.endName;
 
@@ -284,7 +325,8 @@ void fileProcessing (FILE *fd)
 		  trackPointList trPointList;
 		  fread (&trPointList, sizeof (trPointList), 1, fd);
 
-		  readCoordinates (trPointList.size, fd, horCal, verCal);
+		  //readCoordinates (trPointList.size, fd, horCal, verCal);
+		  fseek (fd, trPointList.size, SEEK_CUR);
 
 		  trackPointList events;
 
@@ -365,6 +407,7 @@ chunk returnChunkHeader (FILE *fd)
 	return result;
 }
 
+
 void asessTag (char fileTag[], char Tag[])
 {
 
@@ -391,6 +434,138 @@ int countBytes (FILE *fd)
 	fseek (fd, iniPos, SEEK_SET);
 
 	return size;
+}
+
+void printTrackHeaders (FILE *fd, int tracks, char * fileName)
+{
+  int iniPos = 0;
+  int Ntrack = 0;
+  int pdateTrackEPOCH = 0;
+
+  iniPos = ftell (fd);
+  printf ("Initial pos is %i tracksN is %i\n\n", iniPos, tracks);
+  for (Ntrack=1; Ntrack <= tracks; Ntrack +=1)
+  {
+	int dateTrackEPOCH=0;
+	char * dateString;
+
+	fprintf (stderr, "Track being printed +++++++++++++++++++++++++ : %d \n\n\n", Ntrack);
+
+    trackInfo trInfo;
+    fread (&trInfo, sizeof trInfo, 1, fd);
+
+    fprintf (stderr, "TR id: %s \n", trInfo.id);
+    fprintf (stderr, "TR size: %d \n", trInfo.size);
+    fprintf (stderr, "TR listType: %s \n", trInfo.listType);
+
+    trackFormat trFormat;
+    fread (&trFormat, sizeof trFormat, 1, fd);
+
+    fprintf (stderr, "TR format id: %s \n", trFormat.id);
+    fprintf (stderr, "TR format size: %d \n", trFormat.size);
+
+    int version = 0;
+	char endExperimenter[1] = "";
+	char experimenter[35] = "";
+	char endIdAnimal[1] = "" ;
+	char idAnimal[23] = "";
+	double dateTrack = 0;
+	int NTrackFile = 0;
+	float samplingTime = 0;
+	char endcalUnit[1] = "";
+	char calUnit[10] = "";
+	float horCal = 0;
+	float verCal = 0;
+	int NPoints = 0;
+	char dump[1];
+
+	fread (&version, sizeof (version), 1, fd);
+	fread (&endExperimenter, sizeof (endExperimenter), 1, fd);
+	fread (&experimenter, sizeof (experimenter), 1, fd);
+	fread (&endIdAnimal, sizeof (endIdAnimal), 1, fd);
+	fread (&idAnimal, sizeof (idAnimal), 1, fd);
+	fread (&dateTrack, sizeof (dateTrack), 1, fd);
+	fread (&NTrackFile, sizeof (NTrackFile), 1, fd);
+	fread (&samplingTime, sizeof (samplingTime), 1, fd);
+	fread (&endcalUnit, sizeof (endcalUnit), 1, fd);
+	fread (&calUnit, sizeof (calUnit), 1, fd);
+	fread (&horCal, sizeof (horCal), 1, fd);
+	fread (&verCal, sizeof (verCal), 1, fd);
+	fread (&NPoints, sizeof (NPoints), 1, fd);
+	fread (&dump, sizeof (dump), 1, fd);
+
+	//(fread (&version, sizeof version, 1, trHeaderBuf);
+    //printf ("Enter sentence to append: ");
+
+	//chivatos
+	fprintf (stderr, "TR header version: %d \n", version);//del
+	fprintf (stderr, "TR header endExperimenter: %d \n", endExperimenter[0]);//del
+	fprintf (stderr, "TR header endExperimenter: %s \n", experimenter);//del
+	fprintf (stderr, "TR header trackFile: %d \n", NTrackFile );
+
+	dateTrackEPOCH = excelTime2EPOCH (dateTrack);
+	dateString = returnTimeString (dateTrack, 80);
+	//int dateString = excelTime2EPOCH (dateEPOCH);
+
+	if (dateTrackEPOCH != pdateTrackEPOCH && Ntrack != 1)
+	{
+	  fprintf (stderr, "ERROR: %i time stamps in tracks are not all equal %i\n", dateTrackEPOCH, pdateTrackEPOCH);
+	   exit (1);
+	}
+
+	fprintf (stderr, "Creation date EPOCH format: %i\n", dateTrackEPOCH);
+
+	/*Starting time is code in each track so first time is printed in the header and the coincidence with all the
+	following tracks time is checked */
+
+	if (Ntrack == 1)
+	{
+	  fprintf (stdout, "#h;%s;HEADER;EHEADER;StartStamp;%i\n", fileName, dateTrackEPOCH);
+	  fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Track Date & Time;%s", fileName,Ntrack, dateString);
+	}
+	else
+	{
+	  fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Track Date & Time;%s", fileName,Ntrack, dateString);
+	}
+
+	pdateTrackEPOCH = dateTrackEPOCH;
+	trackRemarksFormat trRmFormat;
+	fread (&trRmFormat, sizeof (trRmFormat), 1, fd);
+	//fprintf (stdout, "--------------------size %i\n",trRmFormat.size);
+	fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Comments;%s\n", fileName, Ntrack, trRmFormat.remarks);
+	fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Number of Samples;%i\n", fileName, Ntrack, NPoints);
+	fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Calibration Units;%s\n", fileName, Ntrack, calUnit);
+	fprintf (stdout, "#h;%s;TRACK INFORMATION;%i;Experimenter;%s\n", fileName, Ntrack, experimenter);
+	puts ("IWH");
+
+	trackPointList dummyTrPointList;
+	fread (&dummyTrPointList, sizeof (dummyTrPointList), 1, fd);
+
+	//readCoordinates (trPointList.size, fd, horCal, verCal);
+
+	//fprintf (stdout, "--------------------size %i\n",DummyTrPointList.size);
+	fseek (fd, dummyTrPointList.size, SEEK_CUR);
+
+	trackPointList dummyEvents;
+
+	fread (&dummyEvents, sizeof(dummyEvents), 1, fd);
+
+	fprintf (stderr, "event id %s\n", dummyEvents.id);
+	fprintf (stderr, "event size %d\n", dummyEvents.size);
+
+    fseek (fd, dummyEvents.size, SEEK_CUR);
+
+  }
+
+  //returning file to original pos
+  fseek (fd, iniPos, SEEK_SET);
+
+
+  int endPos = 0;//#del
+  endPos = ftell (fd);//#del
+  printf ("End pos is %i\n\n", endPos);//#del
+//  //return 0;
+
 }
 
 void readCoordinates (int size, FILE *fd, float hCal, float vCal)
@@ -434,4 +609,20 @@ int excelTime2EPOCH (double timeTrack)
     timeEPOCH -= 3600; //Conversion to GTM value, perl functions implemented convert to EPOCH time GTM.
 
     return timeEPOCH;
+}
+
+char * returnTimeString (double EPOCHseconds, int n)
+{
+   char * dateOut;
+   dateOut = malloc (n * sizeof (char));
+   time_t time2time_t;
+   struct tm * creationFileEPOCH;
+
+   time2time_t = excelTime2EPOCH (EPOCHseconds);
+   creationFileEPOCH = localtime (&time2time_t);
+		  //14/05/2010 15:57:44
+
+   strftime (dateOut, 80, "%d/%m/%Y %X\n", creationFileEPOCH);
+   return dateOut;
+
 }
