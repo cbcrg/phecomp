6e8a1a5f063624e861999f307b521b31255aa1d3
Jose Espinosa <kadomu@gmail.com>
2014-05-16 18:11:10 +0200

    [phecomp][int2browser.pl] adding the color gradient when channels are split (food_fat_1, food_fat_2,...)

--- lib/perl/int2browser.pl
+++ lib/perl/int2browser.pl
@@ -104,6 +104,10 @@ our @intervals = ("0.03", "0.04", "0.05", "0.06", "0.07", "0.08", "0.09", "1", "
 
 our $colorsGrad = {};
 $colorsGrad ->{"water"} = [@blueGradient];
+$colorsGrad ->{"water_1"} = [@blueGradient];
+$colorsGrad ->{"water_2"} = [@blueGradient];
+$colorsGrad -> {"food_fat_1"} = [@redGradient];
+$colorsGrad -> {"food_fat_2"} = [@redGradient];
 $colorsGrad ->{"drink"} = [@blueGradient];
 $colorsGrad -> {"food"} = [@blackGradient];
 #$colorsGrad -> {"food_sc"} = [@blackGradient];
