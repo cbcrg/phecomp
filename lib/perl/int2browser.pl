ed4284ae7ae04cc2d621b523b7dd544e043d93fb
JoseEspinosa <kadomu@gmail.com>
2014-06-04 18:13:48 +0200

    [phecomp][int2browser.pl] some comments

--- lib/perl/int2browser.pl
+++ lib/perl/int2browser.pl
@@ -2720,6 +2720,7 @@ sub bin
 # 2 for presence of food
 # 3 for presence of water and food at the same time
 # One rhmm file with all the cages, this way I can directly feed rhmm.pl
+# It first do a simple binning of 1 and 0 and then is combine for example 1+2 = 3 (drink + food)
 sub writeWindowBinningSingleHmmFile 
   {
     my $h = shift;
@@ -2876,6 +2877,7 @@ sub writeWindowBinningSingleHmmFile
   }
    
 #This function writes the binning of the data in a single rhmm file with all cages and channels in a single file   
+#2 bins 1 and 0 
 sub writeWindowBinarySingleHmmFile 
   {
     my $h = shift;
