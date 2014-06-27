9995fc993f4c2ac9d3f34cb38cdae4509a37f9c0
Jose Espinosa <kadomu@gmail.com>
2014-06-27 10:37:33 +0200

    [phecomp][splitIntfilesByWin.pl] script for spliting int files in equal files of the size of a given window

---
+++ lib/bash/splitIntFilesByWin.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#################################################################################
+### Jose Espinosa-Carrasco. CB/CSN-CRG. May 2014                              ### 
+#################################################################################
+### Code : 27.06                                                              ###
+### This script split int files into equal window size files.                 ### 
+### This way the probability of each window can be calculated using rHMM.pl   ### 
+###                                                                           ###
+###                                                                           ###
+###                                                                           ###
+#################################################################################
+
+## Export the environment
+#$ -V
+file=$1
+
+# Smallest file will have 2 hours of experiment
+# As the binning is of 300 seconds --> 2 h = 24 lines of file
+ws=300
+splitSize=2
+stepSize=$(( splitSize * 3600 / ws ))
+
+linesFile=$(wc -l < "$file")
+lastLine=`cat $file | tail -1`
+cage=${lastLine##*cage;}
+fileName=`echo $fileAndExt | cut -d . -f1`
+cage=${cage%;chN*}
+
+for winSize in $(seq $stepSize $stepSize $linesFile)
+do
+	echo $winSize
+	sed -n "1,$winSize p" $file > "c"_$cage"_s_"$winSize
+done
