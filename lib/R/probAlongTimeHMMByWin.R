62e1b42e03e33384887a2fd5a1883641b80925ca
Jose Espinosa <kadomu@gmail.com>
2014-06-29 23:02:15 +0200

    [phecomp][probAlongTimeHMMByWin.R] script to plot the probability estimated during windows of time

---
+++ lib/R/probAlongTimeHMMByWin.R
@@ -0,0 +1,10 @@
+##################################################################
+### Jose A Espinosa. CSN/CB-CRG Group. Jun 2014                ###
+##################################################################
+### This script plots the probability of each cage along time  ###
+### in window of time.                                         ###
+### I use data to estimate a model for example habituation     ###
+### then I calculate the probability of a sequence of events   ###
+### produced by each animal during a given window time (for    ###
+### example 2 hours, 12 hours or 24 hours                      ###
+##################################################################
