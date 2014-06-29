1eed1ed26110df554eca789df6546b3f4a7a5ebf
Jose Espinosa <kadomu@gmail.com>
2014-06-29 23:23:45 +0200

    [phecomp][probAlongTimeHMM.R] changes in the plot

--- lib/R/probAlongTimeHMM.R
+++ lib/R/probAlongTimeHMM.R
@@ -16,34 +16,46 @@ source ("/Users/jespinosa/git/phecomp/lib/R/plotParamPublication.R")
 home <- Sys.getenv("HOME")
 
 path2Tbl <- "/phecomp/20140512_probOfIncreasingInt/20120502_FDF_habDevW1_W2/prob/tableResults.tbl"
-path2Tbl <- "/phecomp/20140512_probOfIncreasingInt/20130130_FCSC_habDevW1_W2/prob/tableResults.tbl"
+# path2Tbl <- "/phecomp/20140512_probOfIncreasingInt/20130130_FCSC_habDevW1_W2/prob/tableResults.tbl"
 df.probSeq <- read.table (paste (home, path2Tbl, sep = ""), sep="\t", dec=".", header=T, stringsAsFactors=T)
 head (df.probSeq)
+df.probSeq$time <- df.probSeq$step * 300 / (3600*24) 
 
-# df.probSeq <- labelGroups (df.probSeq)
-df.probSeq <- labelGroups (df.probSeq, ctrlGroup = "even", labelCase = "FC diet")
+# For high-fat food
+df.probSeq <- labelGroups (df.probSeq)
+
+# For free-choice
+# df.probSeq <- labelGroups (df.probSeq, ctrlGroup = "even", labelCase = "FC diet")
 
 df.probSeq$cage <- as.factor (df.probSeq$cage)
 df.probSeq$diet <- as.factor (df.probSeq$diet)
-probLinesPlot <- ggplot(df.probSeq, aes(x=step, y=evalScore, group=cage)) + geom_line(aes(colour=diet), size = 1.5) +
-                 scale_color_manual (values=c("red", "darkgreen"))
+probLinesPlot <- ggplot(df.probSeq, aes(x=time, y=evalScore, group=cage)) + geom_line(aes(colour=diet), size = 1.5) +
+                 scale_color_manual (values=c("red", "darkgreen")) +
+                 labs (y = "score log(p)") + labs (x = "time (days)")
+
 probLinesPlot
 # Adding cage number
-df.probSeqLastPoint <- df.probSeq [which (df.probSeq$step == max(df.probSeq$step)),]
+df.probSeqLastPoint <- df.probSeq [which (df.probSeq$time == max(df.probSeq$time)),]
 df.probSeqLastPoint
-bigPlot <- probLinesPlot + geom_text (data = df.probSeqLastPoint, aes (x =step+200, y =evalScore, label = cage), size=6, face="bold")
-                           
-df.probSeqSmall <- df.probSeq [which (df.probSeq$step > 6000), ]
-smallPlot <- ggplot(df.probSeqSmall, aes(x=step, y=evalScore, group=cage)) + 
+# bigPlot <- probLinesPlot + geom_text (data = df.probSeqLastPoint, aes (x =time+1, y =evalScore, label = cage), size=6, face="bold")
+bigPlot <- probLinesPlot                           
+
+# For free choice
+# df.probSeqSmall <- df.probSeq [which (df.probSeq$step > 6000), ]
+
+# For high-fat
+df.probSeqSmall <- df.probSeq [which (df.probSeq$step > 4500), ]
+
+smallPlot <- ggplot(df.probSeqSmall, aes(x=time, y=evalScore, group=cage)) + 
              geom_line(aes(colour=diet), size = 1.5) +
              scale_color_manual (values=c("red", "darkgreen"))
 smallPlot
 smallPlot <- smallPlot + theme (panel.border = element_blank()) +
-  labs (y = NULL, x=NULL) + #scale_x_continuous (breaks = NA) +
+  labs (y = NULL, x=NULL) +
   scale_y_continuous(breaks = NULL) +
-  scale_x_continuous (breaks = NULL)+
-  theme (legend.position = "none") +
-  labs(title = "Means") 
+#   scale_x_continuous (breaks = NULL)+
+  theme (legend.position = "none", axis.text.x = element_text (size=12), plot.title = element_text (size=12)) +
+  labs(title = "Last period") 
   
 # smallPlotLines <- smallPlotLines + geom_text (data = df.probSeqLastPoint, aes (x =step+200, y =evalScore, label = cage), size=6, face="bold")
 # smallPlotLines
@@ -52,8 +64,9 @@ bigPlot
 
 ## Combining the 2 plots in the same figure
 # Placing and setting the size of the subplot
-vp <- viewport (width = 0.45, height = 0.30, x = 0.50,
-                y = unit (45, "lines"), just = c ("left",
+vp <- viewport (width = 0.45, #height = 0.30, x = 0.50,
+                height = 0.4, x = 0.55,
+                y = unit (27, "lines"), just = c ("left",
                                                   "top"))
 
 full <- function() 
@@ -64,3 +77,4 @@ full <- function()
 }
 full ()
 
+chr1:820,000-1,508,013
