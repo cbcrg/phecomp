e172422099fc0e532f2b35f2ee983ab1f0e9246e
Jose Espinosa <kadomu@gmail.com>
2014-06-30 14:45:25 +0200

    [phecomp][probAlongTimeHMMByWin.R] adding separation on phases win=12h

--- lib/R/probAlongTimeHMMByWin.R
+++ lib/R/probAlongTimeHMMByWin.R
@@ -13,6 +13,8 @@
 home <- Sys.getenv ("HOME")
 wd<-getwd()
 
+setwd ("/Users/jespinosa/phecomp/20140627_probOfWindowInt")
+
 ##Libraries
 library (ggplot2)
 library (plyr)
@@ -39,6 +41,18 @@ labelGroups <- function (df.data, ctrlGroup = "odd", labelCase = "HF diet", labe
   return (df.data)
 }
 
+## HIGH FAT
+#Table window of 2 hours
+path2Tbl <- ""
+windowSize <- "2"
+#Table window of 12 hours
+path2Tbl <- "/phecomp/20140627_probOfWindowInt/20120502_FDF_habDevW1_W2_step12h/prob/tableResults.tbl"
+windowSize <- "12"
+#Table window of 24 hours
+path2Tbl <- "/phecomp/20140627_probOfWindowInt/20120502_FDF_habDevW1_W2_step24h/prob/tableResults.tbl"
+windowSize <- "24"
+
+## FREE-CHOICE
 #Table window of 2 hours
 path2Tbl <- "/phecomp/20140627_probOfWindowInt/20130130_FCSC_habDevW1_W2_step2h/prob/tableResults.tbl"
 windowSize <- "2"
@@ -52,15 +66,19 @@ windowSize <- "24"
 df.probWin <- read.table (paste (home, path2Tbl, sep = ""), sep="\t", dec=".", header=T, stringsAsFactors=T)
 
 head (df.probWin)
+# time in days
 df.probWin$time <- df.probWin$step * 300 / (3600*24)
 
 ## Labeling of the groups 
 # HIGH FAT
-# df.probWin <- labelGroups (df.probWin)
+df.probWin <- labelGroups (df.probWin)
+group = "High-Fat Diet"
+title = paste ("Model estimated on habituation,\nposterior probability on windows of", windowSize, "hours\n", group, "\n", sep =" " )
+
 # FREE-CHOICE
 df.probWin <- labelGroups (df.probWin, ctrlGroup = "even", labelCase = "FC diet")
-group = "Free-choice diet"
-title = paste ("Model estimated on habituation,\nposteiror probability windows of", windowSize, "hours\n", group, "\n", sep =" " )
+group = "Free-Choice Diet"
+title = paste ("Model estimated on habituation,\nposterior probability on windows of", windowSize, "hours\n", group, "\n", sep =" " )
 
 
 df.probWin$cage <- as.factor (df.probWin$cage)
@@ -69,7 +87,7 @@ df.probWin$diet <- as.factor (df.probWin$diet)
 probLinesPlot <- ggplot(df.probWin, aes(x=time, y=evalScore, group=cage)) + geom_point(aes(colour=diet), size = 4) +
   scale_color_manual (values=c("red", "green")) +
   labs (y = "score log(p)\n") + labs (x = "\ntime (days)") + labs (title = title)
-probLinesPlot + theme (plot.background = element_rect(fill = 'black', colour = 'red'),
+probLinesPlot + theme (plot.background = element_rect(fill = 'black', colour = 'black'),
                        axis.title.x = element_text (color='white'),
                        axis.title.y = element_text (color='white'),
                        plot.title = element_text (color='white', size=base_size * 2),
@@ -80,6 +98,28 @@ probLinesPlot + theme (plot.background = element_rect(fill = 'black', colour = '
                        legend.background = element_rect(fill = 'black'),
                        legend.key = element_rect(fill = 'black') 
                        ) 
-                
-        
+
+## Two plots one with light periods and one with night periods
+# Only when we are working with step size = 12 h
+df.probWinStep12h <- df.probWin       
+df.probWinStep12h$time - as.integer (df.probWinStep12h$time)
             
+df.probWinStep12h$phase <-  sapply (df.probWinStep12h$time, y <- function (x) { if (x - as.integer (x) == 0) return ("Dark phase") else {return ("Light phase")}})
+df.probWinStep12h$dietPhase <- paste (df.probWinStep12h$diet, df.probWinStep12h$phase, sep="-")
+probLinesPlot <- ggplot(df.probWinStep12h, aes(x=time, y=evalScore, group=cage)) + geom_point(aes(colour=dietPhase), size = 4) +
+  scale_color_manual (values=c("red", "deeppink1", "darkgreen" , "chartreuse")) +
+  labs (y = "score log(p)\n") + labs (x = "\ntime (days)") + labs (title = title)
+
+probLinesPlot + theme (plot.background = element_rect(fill = 'black', colour = 'black'),
+                       axis.title.x = element_text (color='white'),
+                       axis.title.y = element_text (color='white'),
+                       plot.title = element_text (color='white', size=base_size * 2),
+                       axis.text.x = element_text (color='white'),
+                       axis.text.y = element_text (color='white'),                       
+                       legend.title = element_text (color='white'),
+                       legend.text = element_text (color='white'),
+                       legend.background = element_rect(fill = 'black'),
+                       legend.key = element_rect(fill = 'black') 
+) 
+colors()[117]
+colors()[47]
