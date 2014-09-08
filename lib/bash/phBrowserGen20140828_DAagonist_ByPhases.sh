78aea2fac8b95dac8e15177b43ccabf762ed708b
JoseEspinosa <kadomu@gmail.com>
2014-09-08 17:43:48 +0200

    [phecomp][phBrowserGen20140828_DAagonist_ByPhases.sh] split data in10 min windows

--- lib/bash/phBrowserGen20140828_DAagonist_ByPhases.sh
+++ lib/bash/phBrowserGen20140828_DAagonist_ByPhases.sh
@@ -41,3 +41,6 @@ qsub -q $tcypeQ,$nameQ $timeQ -cwd -o ${mtbLogFolder} -e ${mtbLogFolder} -v aryM
 
 # each 15 min
 qsub -q $tcypeQ,$nameQ $timeQ -cwd -o ${mtbLogFolder} -e ${mtbLogFolder} -v aryMtbFiles="${aryMtbFilesDev[*]}" -v dumpDir="/users/cn/jespinosa/phecomp/processedData/" -v iniLight=6  -v phaseTag="_all_30min" -v par2int2browser="value"  -v winSize=900 ${bashScDir}mtb2GBNoQsubIn.sh
+
+# each 10 min
+qsub -q $tcypeQ,$nameQ $timeQ -cwd -o ${mtbLogFolder} -e ${mtbLogFolder} -v aryMtbFiles="${aryMtbFilesDev[*]}" -v dumpDir="/users/cn/jespinosa/phecomp/processedData/" -v iniLight=6  -v phaseTag="_all_30min" -v par2int2browser="value"  -v winSize=600 ${bashScDir}mtb2GBNoQsubIn.sh
