3fdc8c5504ed9f387a736bb5c8a635ded5dba5a1
Jose Espinosa <kadomu@gmail.com>
2014-09-30 16:45:52 +0200

    [pergola][executionExamples.py] script cleaned with main possible exec

--- lib/python/tests/executionExamples.py
+++ lib/python/tests/executionExamples.py
@@ -44,215 +44,202 @@ configFileDict = int2browser.ConfigInfo(configFilePath)
 
 ###################
 ## Config file dictionary printed 
-# print (configFileDict)
-# print ("************")
-# print (type (configFileDict.correspondence))
-# configFileDict.write()
-# 
-# print ("=============")
+print ("Config file: %s\n" % (type(configFileDict)))
+print (type (configFileDict.correspondence))
+print ("Dictionary config:")
+configFileDict.write()
 
 ## How debugging file is created
 ## Input debugging file
 #cat 20120502_FDF_CRG_hab_filtSHORT.csv | sed 's/ //g' | awk '{print $1"\t"$14"\t"$6"\t"$11"\t"$16"\thabituation"}' > shortDev.int
 
-# print (path)
-
 ## Generation of a genome file
-intData = int2browser.intData (path, ontology_dict=configFileDict.correspondence,relative_coord=True)
-
-# print (type(intData.min))
-# print (intData.min)
+intData = int2browser.intData (path, ontology_dict=configFileDict.correspondence, relative_coord=True)
+print ("Min value in the file %i" % (intData.min))
 
 ## Checking type
 # Now before applying read it has not class data_iter ask someone whether this is ok or not
-# print(type(intData))
+print("Type intData %s" % (type(intData)))
 intData_data_iter = intData.read()
-# print(type(intData_data_iter))
+print("Type intData after method read applied %s" % type(intData_data_iter))
 
+######################
 ## Writing chromosome file
-intData.writeChr()
+# intData.writeChr()
 
-## Convert to bed
-# bed_str = intData.convert(mode = "bed", relative_coord = True)
 
-# separating by data types (nature)
-# bed_str =  intData.convert(mode = "bed", relative_coord = True, split_dataTypes=True)
-bed_str = intData.convert(mode = "bedGraph", relative_coord=True, split_dataTypes=True, window=300)
+######################
+## Convert to BED
+# split_dataTypes=False by default
+bed_str = intData.convert(mode = "bed", relative_coord = True)
+print ("Type after conversion is %s" % type(bed_str[('2', 'water_food_sc')]))
+                     
+# separating by data types (nature) split_dataTypes=True
+bed_str =  intData.convert(mode = "bed", relative_coord = True, split_dataTypes=True)
+print("Type after conversion is %s" % type(bed_str[('2', 'water')]))
 
-# Class of each object of the dictionary
-print (type(bed_str[('2', 'water')]))
-
-for key in bed_str: 
-    print (key), 
-    print ("---------")
+for key in bed_str:     
     bedSingle = bed_str[key]
+#     for line in bedSingle: print line
     name_file='_'.join(key)
-    print ("Name of the file generated is %s" % (str(key)))
-    
-##Utilizar split_dataTypes como variable para guardar como queremos que se distribuyan los datos
+    # Writing the file 
+    # file_type might be interesting that the type of file is automatically recognized
+#     bedSingle.write(track=name_file, file_type="bed")
+    print("Name of the file generated is %s" % (name_file))
 
-#    print ("---------")
+######################
+## Convert to BEDGRAPH
+# bedGraph_str = intData.convert(mode = "bedGraph", relative_coord=True, split_dataTypes=True, window=300)
+bedGraph_str = intData.convert(mode = "bedGraph", relative_coord=True, split_dataTypes=True, window=1800)
+print ("Type after conversion is %s" % type(bedGraph_str[('2', 'water')]))
+
+for key in bedGraph_str:     
+    bedSingle = bedGraph_str[key]
+    for line in bedSingle: print line
+    name_file='_'.join(key)
+    # Writing the file 
+    # file_type might be interesting that the type of file is automatically recognized
+#     bedSingle.write(track=name_file, file_type="bedGraph")
+    print("Name of the file generated is %s" % (name_file))
+
+
+#     
+# ##Utilizar split_dataTypes como variable para guardar como queremos que se distribuyan los datos
+
+
+
+
+
+# ##################
+# # 
+# # print ("===============")
+# # d_rest_colors = {'water' : 'green'}
+# # set_dataTypes = intData.dataTypes
+# # print set_dataTypes
+#   
+# # print (int2browser.assign_color (set_dataTypes, _dict_rest_colors))
+# 
+# # print (int2browser.assign_color (set_dataTypes))
+# ##########################
+# ## Examples of executions 
+#          
+# # intData = intData(path, relative_coord=True)
+# # print ("===============")
+# # _dict_rest_colors = {
+# #                       'water' : 'blue'}
+# # set_dataTypes = intData.dataTypes
+# # print set_dataTypes
+# # 
+# # assign_color (set_dataTypes, _dict_rest_colors)
+# 
+# # print (intData.get_field_items("dataTypes"))
+# # for row in intData.read(relative_coord=True):
+# #     print row
+# # print(intData.min)
+# 
+#  
+# intData.convert(mode = "bed", relative_coord = True)   
+# # bedFiles = intData.convert(mode = "bed", relative_coord = True, split_dataTypes=False, restrictedColors=d_rest_colors)
+# bedFiles=intData.convert(mode = "bedGraph", window=300, split_dataTypes=False, relative_coord=True)
+#  
+# for key in bedFiles: 
+#     print (key), 
+#     print ("---------")
+#     bedSingle = bedFiles[key]
+#     name_file='_'.join(key)
+#     print ("---------")
 #     print (name_file)
 #     bedSingle.write(track=name_file, file_type="bedGraph")
 #     for line in bedSingle: print line
-
-# with open("input.txt", "r") as handle:
-#     lookup = dict((x[2], x[1]) for x in (x.split('t') for x in handle.read().split('n') if x))
-
-
-
-# with open(filename) as config_file:
-#     culo=dict((row[0], row[1]) for row in (csv.reader(config_file, delimiter='\t')))
-# #     list_of_dicts = list(csv.DictReader(file_object,delimiter='\t'))
-#     
-# print culo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################
-# 
-# print ("===============")
-# d_rest_colors = {'water' : 'green'}
-# set_dataTypes = intData.dataTypes
-# print set_dataTypes
-  
-# print (int2browser.assign_color (set_dataTypes, _dict_rest_colors))
-
-# print (int2browser.assign_color (set_dataTypes))
-##########################
-## Examples of executions 
-         
-# intData = intData(path, relative_coord=True)
-# print ("===============")
-# _dict_rest_colors = {
-#                       'water' : 'blue'}
-# set_dataTypes = intData.dataTypes
-# print set_dataTypes
-# 
-# assign_color (set_dataTypes, _dict_rest_colors)
-
-# print (intData.get_field_items("dataTypes"))
-# for row in intData.read(relative_coord=True):
-#     print row
-# print(intData.min)
-
- 
-intData.convert(mode = "bed", relative_coord = True)   
-# bedFiles = intData.convert(mode = "bed", relative_coord = True, split_dataTypes=False, restrictedColors=d_rest_colors)
-bedFiles=intData.convert(mode = "bedGraph", window=300, split_dataTypes=False, relative_coord=True)
- 
-for key in bedFiles: 
-    print (key), 
-    print ("---------")
-    bedSingle = bedFiles[key]
-    name_file='_'.join(key)
-    print ("---------")
-    print (name_file)
-    bedSingle.write(track=name_file, file_type="bedGraph")
-    for line in bedSingle: print line
-  
-#  
-#         
-# s=intData.read(relative_coord=True)
 #   
-# bedFile = intData.track_convert2bed(s)
-# for line in bedFile:    
-#     print (line) 
-
-
-
-
-
-
-# for key in bedFiles:
-#     bed = bedFiles [key]
-#     print ("bed file" + key)
-#     for line in bed:  print line
-
-# s = intData.read(relative_coord = True, )
-
-# print (s.fields)
-# write(s)
-
-# print (intData.get_min_max(fields=["dataValue", "dataValue"]))
-# print (intData.tracks)
-# print (intData.dataTypes)
-
-# s2=write(s)
-# for line in s2: print line
-# print (type (s))
-# d=iter(s)
-# n=d.next()
-# print n
-# i = dataIter(s)
-# int=i.next()
-# print int
-# if isinstance(s,(list,tuple)):
-#     print "culo"
-# else:
-#     print "pedo"    
-# Tengo que crear las clases de los objetos correspondientes a cada tipo de datas bed, bedgraph y si hay alguno mas
-# Tendran un metodo para hacer write que puede ser diferente segun el tipo o directamente si lo hago bien, simplemente
-# cogera las lineas y sera capaz de hacerlo con una funcion generica
-# intDataBed = intData.writeBed (feature="dataValue")
+# #  
+# #         
+# # s=intData.read(relative_coord=True)
+# #   
+# # bedFile = intData.track_convert2bed(s)
+# # for line in bedFile:    
+# #     print (line) 
 # 
-# for line in intDataBed:
-#     for item in line:
-#         print (item),  
-#     print ('\n'),
-
-
-# Stuff that might be interesting
-#import numpy as np
-#A = np.array (dataInt)
-
-#def genGenome (endChr, "N"):
-  
-## Example with pandas http://stackoverflow.com/questions/16503560/read-specific-columns-from-csv-file-with-python-csv
-#import pandas as pd
-#df = pd.read_csv (args.input)
-#col = df [chromStart]
-#print '------%-4s' % (col)
-
-#pybedtools examples
-# import pybedtools
-# x = pybedtools.BedTool('path/to/bam')
-# x.genome_coverage(bg=True, genome='hg19', split=True)\
-#     .saveas('path/to/bedgraph', trackline='track name="test track" visibility="full" type=bedGraph')
-        
-
-
-# data=[(1, 'A', 'foo'),
-#     (2, 'A', 'bar'),
-#     (100, 'A', 'foo-bar'),
-#      (300, 'A', 'foo-bar'),
-#  
-#     ('xx', 'B', 'foobar'),
-#     ('yy', 'B', 'foo'),
-#     ('yx', 'B', 'foo'),
-#     (500, 'A', 'foo-bar'),
-#      
-#     (1000, 'C', 'py'),
-#     (200, 'C', 'foo'),
-#     ]
-#
-# data2=sorted(data,key=operator.itemgetter(2))
-
-  
-# for key,group in itertools.groupby(data2,operator.itemgetter(1,2)):
-#     print(tuple(group))
-#     print key
+# 
+# 
+# 
+# 
+# 
+# # for key in bedFiles:
+# #     bed = bedFiles [key]
+# #     print ("bed file" + key)
+# #     for line in bed:  print line
+# 
+# # s = intData.read(relative_coord = True, )
+# 
+# # print (s.fields)
+# # write(s)
+# 
+# # print (intData.get_min_max(fields=["dataValue", "dataValue"]))
+# # print (intData.tracks)
+# # print (intData.dataTypes)
+# 
+# # s2=write(s)
+# # for line in s2: print line
+# # print (type (s))
+# # d=iter(s)
+# # n=d.next()
+# # print n
+# # i = dataIter(s)
+# # int=i.next()
+# # print int
+# # if isinstance(s,(list,tuple)):
+# #     print "culo"
+# # else:
+# #     print "pedo"    
+# # Tengo que crear las clases de los objetos correspondientes a cada tipo de datas bed, bedgraph y si hay alguno mas
+# # Tendran un metodo para hacer write que puede ser diferente segun el tipo o directamente si lo hago bien, simplemente
+# # cogera las lineas y sera capaz de hacerlo con una funcion generica
+# # intDataBed = intData.writeBed (feature="dataValue")
+# # 
+# # for line in intDataBed:
+# #     for item in line:
+# #         print (item),  
+# #     print ('\n'),
+# 
+# 
+# # Stuff that might be interesting
+# #import numpy as np
+# #A = np.array (dataInt)
+# 
+# #def genGenome (endChr, "N"):
+#   
+# ## Example with pandas http://stackoverflow.com/questions/16503560/read-specific-columns-from-csv-file-with-python-csv
+# #import pandas as pd
+# #df = pd.read_csv (args.input)
+# #col = df [chromStart]
+# #print '------%-4s' % (col)
+# 
+# #pybedtools examples
+# # import pybedtools
+# # x = pybedtools.BedTool('path/to/bam')
+# # x.genome_coverage(bg=True, genome='hg19', split=True)\
+# #     .saveas('path/to/bedgraph', trackline='track name="test track" visibility="full" type=bedGraph')
+#         
+# 
+# 
+# # data=[(1, 'A', 'foo'),
+# #     (2, 'A', 'bar'),
+# #     (100, 'A', 'foo-bar'),
+# #      (300, 'A', 'foo-bar'),
+# #  
+# #     ('xx', 'B', 'foobar'),
+# #     ('yy', 'B', 'foo'),
+# #     ('yx', 'B', 'foo'),
+# #     (500, 'A', 'foo-bar'),
+# #      
+# #     (1000, 'C', 'py'),
+# #     (200, 'C', 'foo'),
+# #     ]
+# #
+# # data2=sorted(data,key=operator.itemgetter(2))
+# 
+#   
+# # for key,group in itertools.groupby(data2,operator.itemgetter(1,2)):
+# #     print(tuple(group))
+# #     print key
