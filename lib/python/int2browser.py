cbc1bf09218089c151ad9a7da876733ac53d4e51
JoseEspinosa <kadomu@gmail.com>
2014-10-24 18:20:55 +0200

    [pergola] cleaning code prints

--- lib/python/int2browser.py
+++ lib/python/int2browser.py
@@ -79,7 +79,7 @@ class intData: # if I name it as int I think is like self but with a better name
         self.delimiter = kwargs.get('delimiter',"\t")
         self.delimiter = self._check_delimiter(self.path)
         self.header = kwargs.get('header',True)
-        self.fieldsB = self._set_fields_b(fields=kwargs.get('fields'), ontology_dict=ontology_dict)
+        self.fieldsB = self._set_fields_b(kwargs.get('fields'))
         print "self.fieldsB    :",self.fieldsB
         print "ontology dictionary:       ",ontology_dict
         self.fieldsG = self._set_fields_g(ontology_dict) 
@@ -113,7 +113,7 @@ class intData: # if I name it as int I think is like self but with a better name
             
         return self.delimiter
     
-    def _set_fields_b(self, ontology_dict, fields):
+    def _set_fields_b(self, fields):
         """
         Reading the behavioral fields from the header file or otherwise setting  
         the fields to numeric values corresponding the column index starting at 0    
@@ -143,26 +143,19 @@ class intData: # if I name it as int I think is like self but with a better name
                 fieldsB = fields
                      
             else:       
-                #Attetion if I use open the I would have to get rid of \n
-                #fieldsB=[header[0].strip('# ')]+header[1:-1]+[header[-1][:-1]]
                 fieldsB = [header[0].strip('# ')]+header[1:]        
         else:
             first_r = self.reader.next()
-            print "------------------fields before  are", fields
+            
             if fields:
                 if len(fields) > len(first_r):
                     raise ValueError("Input field list \"%s\" is longer than totals fields available in file \'%s\'" % ("\",\"".join(fields), len(first_r)))            
-                
-                #modify this has to be changed
-                #coger los que quiera de ellos, pero debe ser una lista numerica porque no tengo la
-                #lista de nombres
-                #fieldsB[listOfSelected]
+                            
                 fieldsB = fields
+                
                 print ("WARNING: As header=False you col names set by fields will be consider to have the order "
                         "you provided: %s" 
-                        %"\",\"".join(fields)) 
-                print "------------------fields are", fieldsB
-                
+                        %"\",\"".join(fields))                 
             else:                                
                 raise ValueError ('File should have a header, otherwise you should set ' 
                                   'an ordered list of columns names using fields')     
