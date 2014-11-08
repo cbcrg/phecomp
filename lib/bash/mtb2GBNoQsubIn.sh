7898844fe5b953825581b174b0b4408ad4f1b299
Jose Espinosa <kadomu@gmail.com>
2014-11-08 15:50:05 +0100

    test 2

--- lib/bash/mtb2GBNoQsubIn.sh
+++ lib/bash/mtb2GBNoQsubIn.sh
@@ -216,8 +216,6 @@ checkCreateDir $path2GenBrCombChSign
 checkCreateDir $path2GenBrGroups
 checkCreateDir $path2GenBrTblPh
 
-sleep 10
-
 # Eventually do this in parallel with a qsub call
 errorSplitChGBrowser=${path2GenBrSplit}${expName}"FilterGBrowserSplitCh"".err"
 errorCombChGBrowser=${path2GenBrSplit}${expName}"FilterGBrowserCombCh"".err"
@@ -227,6 +225,7 @@ errorTblPh=${path2GenBrSplit}${expName}"FilterGBrowserTblPh"".err"
 
 # -window Value -iniLight 8 -allFiles genomeBrowser -outdata no -out splitCh "${iniLight}
 # Analised field given by variable field2Window
+								  
 optInt2browserSplitCh=${field2Window}" -allFiles genomeBrowser -outdata no -out splitCh "${iniLight}
 optInt2browserSplitCh=${field2Window}" -allFiles genomeBrowser -ws ${winSize} -wss ${winSize} -outdata no "${iniLight}
 optInt2browserCombCh=${field2Window}" -winMode discrete -winFile combCh -ws ${winSize} -wss ${winSize} -winCh2comb 12,34 -outdata no "${iniLight}
@@ -250,8 +249,13 @@ optInt2browserTblPh=${field2Window}" -winMode discrete -winFile dailyAverage -ws
 # # Samething joining channels
 # qsub -q $typeQ,$nameQ $timeQ -cwd -o ${path2GenBrowser} -e ${path2GenBrowser} ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserTblPhJoinedCh}" ${path2GenBrTblPh} "${errorTblPh}"      
 
-${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserSplitCh}" ${path2GenBrSplit} "${errorSplitChGBrowser}" > ${path2GenBrowser}"GBsplitCh.out" 2> ${path2GenBrowser}"GBsplitCh.err"
+${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserSplitCh}" "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/GBfiles/20141108_hab/splitCh/" "${errorSplitChGBrowser}" > ${path2GenBrowser}"GBsplitCh.out" 2> ${path2GenBrowser}"GBsplitCh.err"
 
+# ${bashScDr}int2browserCallFromQsub.sh "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/intFiles/20140908_TS_WT_FC_CRG_corrected_hab_filt.int" 
+#									  "-window value -allFiles genomeBrowser -outdata no -out splitCh" 
+#									  "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/GBfiles/20141108_hab/splitCh/" 
+#									  "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/GBfiles/20141108_hab/splitCh/test.err"
+	
 ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserCombCh}" ${path2GenBrCombCh} "${errorCombChGBrowser}" > ${path2GenBrowser}"GBcombCh.out" 2> ${path2GenBrowser}"GBcombCh.err"
 
 ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserCombChSign}" ${path2GenBrCombChSign} "${errorCombChSign}" > ${path2GenBrowser}"GBcombSign.out" 2> ${path2GenBrowser}"GBcombSign.err"
