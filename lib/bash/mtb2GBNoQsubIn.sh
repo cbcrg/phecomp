38a114db7db7d4d5881200f0725510a253c02735
Jose Espinosa <kadomu@gmail.com>
2014-11-08 16:29:21 +0100

    back to original version

--- lib/bash/mtb2GBNoQsubIn.sh
+++ lib/bash/mtb2GBNoQsubIn.sh
@@ -225,9 +225,9 @@ errorTblPh=${path2GenBrSplit}${expName}"FilterGBrowserTblPh"".err"
 
 # -window Value -iniLight 8 -allFiles genomeBrowser -outdata no -out splitCh "${iniLight}
 # Analised field given by variable field2Window
-							
+								  
 # optInt2browserSplitCh=${field2Window}" -allFiles genomeBrowser -outdata no -out splitCh "${iniLight}
-optInt2browserSplitCh=${field2Window}"" "${iniLight}" "-allFiles genomeBrowser -ws ${winSize} -wss ${winSize} -outdata no -out splitCh"
+optInt2browserSplitCh=${field2Window}" -allFiles genomeBrowser -ws ${winSize} -wss ${winSize} -outdata no "${iniLight}
 optInt2browserCombCh=${field2Window}" -winMode discrete -winFile combCh -ws ${winSize} -wss ${winSize} -winCh2comb 12,34 -outdata no "${iniLight}
 optInt2browserCombChSign=${field2Window}" -winMode discrete -winFile signCombCh -ws ${winSize} -wss ${winSize} -winCh2comb 12,34 -winCombMode sign -outdata no "${iniLight}
 optInt2browserGroups=${field2Window}" -winMode discrete -winFile groupDistro -ws ${winSize} -wss ${winSize} -winCh2comb 12,34 -winCombMode sign -winCage2comb -caseGroup odd -outdata no "${iniLight}
@@ -251,11 +251,6 @@ optInt2browserTblPh=${field2Window}" -winMode discrete -winFile dailyAverage -ws
 
 ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserSplitCh}" ${path2GenBrSplit} "${errorSplitChGBrowser}" > ${path2GenBrowser}"GBsplitCh.out" 2> ${path2GenBrowser}"GBsplitCh.err"
 
-# ${bashScDr}int2browserCallFromQsub.sh "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/intFiles/20140908_TS_WT_FC_CRG_corrected_hab_filt.int" 
-#									  "-window value -allFiles genomeBrowser -outdata no -out splitCh" 
-#									  "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/GBfiles/20141108_hab/splitCh/" 
-#									  "/users/cn/jespinosa/phecomp/processedData/20140908_TS_WT_FC_CRG_corrected/GBfiles/20141108_hab/splitCh/test.err"
-	
 ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserCombCh}" ${path2GenBrCombCh} "${errorCombChGBrowser}" > ${path2GenBrowser}"GBcombCh.out" 2> ${path2GenBrowser}"GBcombCh.err"
 
 ${bashScDir}int2browserCallFromQsub.sh "${path2IntFileFilter}" "${optInt2browserCombChSign}" ${path2GenBrCombChSign} "${errorCombChSign}" > ${path2GenBrowser}"GBcombSign.out" 2> ${path2GenBrowser}"GBcombSign.err"
