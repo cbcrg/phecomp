200080ac8cefe7bdb9c93bbf266f03a45543f1f2
Jose Espinosa <kadomu@gmail.com>
2015-01-09 11:33:20 +0100

    [phecomp][heatMapFunctions.R] change deprecated elements like theme_text in habituation heatmap function

--- lib/R/heatMapFunctions.R
+++ lib/R/heatMapFunctions.R
@@ -139,9 +139,8 @@ heatMapPlotter <- function (table, main="", weekNotation=F, legPos="right", mode
                            labels = labelsSc,
                            name = legName,
                            rescaler = function(x,...) x,
-                           #                                   oob = identity)+ opts (title = main))#with legend
-                           # oob = identity) + opts (title = main, legend.position = "none"))#no legend
-                           oob = identity) + ggtitle(main) + theme (legend.position = "none"))
+                           #                                   oob = identity)+ ggtitle(main))#with legend                          
+                           oob = identity) + ggtitle(main) + theme (legend.position = "none"))#no legend
   base_size <- 9
   
   p + theme_grey (base_size = base_size) + labs (x = xlab,
@@ -156,7 +155,7 @@ heatMapPlotter <- function (table, main="", weekNotation=F, legPos="right", mode
           axis.text.x = element_text(size = base_size * 1.4, angle = angleY,
                                    #1.2, angle = 330,                     
                                    #hjust = 0, colour = "grey50", face = "bold"), #labels=c("Control", "Treat 1", "Treat 2")),
-                                   hjust = 0, face = "bold"),                           
+                                   hjust = 0, face = "bold", colour = "black"),                           
           #axis.text.y = element_text (size = base_size * 1.2,hjust = 0, colour = "grey50"))
           legend.text = element_text (size = base_size * 1.2),      
           legend.title = element_text (size = base_size *1.2, face = "bold"),      
@@ -164,7 +163,7 @@ heatMapPlotter <- function (table, main="", weekNotation=F, legPos="right", mode
           legend.text = element_text (hjust=c(0, 0.5, 1)),
           plot.title = element_text (size=base_size * 1.5, face="bold"),
           #axis.text.y = element_text (size = base_size * 1.4,hjust = 0, colour = "grey50", face = "bold"))
-          axis.text.y = element_text (size = base_size * 1.4,hjust = 0, face = "bold"))                          
+          axis.text.y = element_text (size = base_size * 1.4,hjust = 0, face = "bold", colour = "black"))                          
 }
 
 #Function for significancy heatmap generation only for HABITUATION PHASE
@@ -252,31 +251,31 @@ heatMapPlotterHab <- function (table, main="", weekNotation=F, legPos="right", m
                            labels = labelsSc,
                            name= legName,
                            rescaler = function(x,...) x,
-                           #                                   oob = identity)+ opts (title = main))#with legend
-                           oob = identity)+ opts (title = main, legend.position = "none"))#no legend
+                           #                                   oob = identity) + ggtitle(main)#with legend
+                           oob = identity) + ggtitle(main) + theme (legend.position = "none"))#no legend
   
   base_size <- 9
   p + theme_grey (base_size = base_size) + labs (x = xlab,
                                                  y = ylab) + scale_x_discrete (expand = c(0, 0)) +
     scale_y_discrete (expand = c(0, 0), labels = table$variable) + 
-    opts (axis.ticks = element_text,
+    theme (axis.ticks = element_blank(),
           legend.position = legPos,
-          panel.border = element_text,
-          panel.background = element_text,
+          panel.border = element_blank(),
+          panel.background = element_blank(),
           axis.title.x =  element_text (size = base_size * 1.4, face = "bold"),
           axis.title.y =  element_text (size = base_size * 1.4, face = "bold", angle = 90),
           axis.text.x = element_text(size = base_size * 1.4, angle = angleY,
                                    #1.2, angle = 330,                     
                                    #hjust = 0, colour = "grey50", face = "bold"), #labels=c("Control", "Treat 1", "Treat 2")),
-                                   hjust = 0, face = "bold"),
+                                   hjust = 0, face = "bold", colour="black"),
           #                           axis.text.x = element_text,
           #axis.text.y = element_text (size = base_size * 1.2,hjust = 0, colour = "grey50"))
           legend.text = element_text (size=base_size * 1.2),      
           legend.title = element_text (size = base_size *1.2, face = "bold"),      
-          legend.hjust = element_text (hjust=c(0, 0.5, 1)),
+          legend.text = element_text (hjust=c(0, 0.5, 1)),
           plot.title = element_text (size=base_size * 1.5, face="bold"),
           #axis.text.y = element_text (size = base_size * 1.4,hjust = 0, colour = "grey50", face = "bold"))
-          axis.text.y = element_text (size = base_size * 1.4,hjust = 0, face = "bold"))                          
+          axis.text.y = element_text (size = base_size * 1.4,hjust = 0, face = "bold", colour = "black"))                          
 }
 
 ##############################
