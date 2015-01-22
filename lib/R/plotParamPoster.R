f6d45097514327d09a1ab67056fbc4c7db8771b1
Jose Espinosa <kadomu@gmail.com>
2015-01-22 11:44:37 +0100

    Adding parameters plotting for poster

---
+++ lib/R/plotParamPoster.R
@@ -0,0 +1,28 @@
+#############################################################################
+### Jose A Espinosa. CSN/CB-CRG Group. July 2014                          ###
+#############################################################################
+### Graphical parameters for ggplot                                       ###
+### Poster quality plots                                                  ### 
+#############################################################################
+
+base_size <- 12
+
+dailyInt_theme <- theme_update (
+  #axis.text.x = element_text (angle = 90, hjust = 1, size = base_size * 1.5),
+  axis.text.x = element_text (hjust = 1, size = base_size * 1.5),
+  axis.text.y = element_text (size = base_size * 1.5),
+  axis.title.x = element_text (size=base_size * 1.5, face="bold"),
+  axis.title.y = element_text (size=base_size * 1.5, angle = 90, face="bold"),
+  strip.text.x = element_text (size=base_size * 1.3, face="bold"),#facet titles size 
+  strip.text.y = element_text (size=base_size * 1.3, face="bold", angle=90),
+  plot.title = element_text (size=base_size * 1.5, face="bold"), 
+  legend.text = element_text (size=base_size * 1.2),             
+  #   panel.grid.major = theme_line (colour = "grey90"),
+  panel.grid.major = element_blank(),
+  #                   panel.grid.minor = element_blank(), 
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  panel.background = element_blank(),
+  axis.line = element_line (colour = "black"),
+  #                   axis.ticks = element_blank())
+  axis.ticks = element_blank())
