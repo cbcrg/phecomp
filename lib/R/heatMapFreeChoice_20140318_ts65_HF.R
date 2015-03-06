f965bbd1a639d42e3d0d439d50adeb9e0ebe1669
Jose Espinosa <kadomu@gmail.com>
2015-03-06 12:12:02 +0100

    Changes in the script to read hab and dev from different files

--- lib/R/heatMapFreeChoice_20140318_ts65_HF.R
+++ lib/R/heatMapFreeChoice_20140318_ts65_HF.R
@@ -20,6 +20,10 @@ source ("/Users/jespinosa/git/phecomp/lib/R/heatMapFunctions.R")
 
 setwd ("/Users/jespinosa/phecomp/20121128_heatMapPhecomp/figures/20140318_TS_CRG_HF")
 
+#########################
+# I have to separate HABITUATION AND DEVELOPMENT INTO DIFFERENT TABLES BECAUSE HABITUATION LAST MORE THAN ONE WEEK
+###########
+# DEVELOPMENT
 ###### 
 ## Load the data
 ## Groups are only separated by genotype WT and ts65dn with same diet HF
@@ -54,7 +58,7 @@ head (df.weekStats)
 tail (df.weekStats,100)
 df.weekStats$channel
 
-### HABITUATION
+### DEVELOPMENT
 #CASE ts65dn
 df.meanCase <- with (df.weekStats [which (df.weekStats$group == caseGroupLabel),] , aggregate (cbind (Number, Avg_Duration, Avg_Intake, Rate, Avg_Intermeal_Duration), list(channel=channel, group=group, period=period), mean))
 
@@ -95,6 +99,92 @@ df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Avg Duration")] <-  "3
 df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Number")] <-  "4"
 df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Avg Intake")] <-  "5"
 
+#Old command to order
+# df.meanCase.m <- df.meanCase.m [with (df.meanCase.m, order (period, channel, variable)),]
+df.meanCase.m <- df.meanCase.m [with (df.meanCase.m, order (period, channel, orderOut) ),]
+
+# Changing label Number by Number of Meals and Number of Drinks
+# To plot significance this must be comment because meal number and drink number won't be recognized #Tag_sig
+# df.meanCase.m$variable [which (df.meanCase.m$variable == "Number" & df.meanCase.m$channel == "food_fat")] <-  "Meal Number"
+# df.meanCase.m$variable [which (df.meanCase.m$variable == "Number" & df.meanCase.m$channel == "water")] <-  "Drink Number"
+
+#############
+#DEVELOPMENT
+#############
+df.meanCase.m.Dev <- df.meanCase.m
+df.meanCase.m.Dev$period <- df.meanCase.m.Dev$period + 1
+heatMapPlotter (df.meanCase.m.Dev, main="\n",  weekNotation="N", legPos="right",
+                xlab="\nDevelopment Phase (weeks)", ylab="Eating Behavior                          Drinking Behavior\n")
+
+ggsave("20140318_ts65_HF_heatMap_dev.pdf", width=10, height=8)
+# ggsave("20140318_ts65_HF_heatMap_dev.png", width=10, height=8, dpi=100)
+
+#############
+#HABITUATION
+#############
+
+weekStatsData <- "/phecomp/20121128_heatMapPhecomp/tblFiles/20140318_TS_CRG_HF_corrected_hab_filtTwoMinFilt.tbl"
+
+df.weekStats <- read.table (paste (home, weekStatsData, sep = ""), sep="\t", dec=".", header=T, stringsAsFactors=F)
+
+#Hard code
+caseGroupLabel <- "ts65dn"  # CASE    == ts65dn
+controlGroupLabel <- "wt"   # CONTROL == wt 
+
+nAnimals <- 18
+#Label by experimental group (control, free choice, force diet...)
+cage <- c (1 : nAnimals)
+group <- c (rep (controlGroupLabel, nAnimals/2), rep (caseGroupLabel, nAnimals/2))
+df.miceGroup <- data.frame (cage, group)
+df.miceGroup$group [c(1, 3, 5, 7, 9, 11, 13, 15, 16, 17, 18)] <- controlGroupLabel
+df.miceGroup$group [c(2, 4, 6, 8, 10, 12, 14, 16)] <- caseGroupLabel
+
+# Joining labels
+df.weekStats <- merge (df.weekStats, df.miceGroup, by.x= "cage", by.y = "cage")
+head (df.weekStats)
+tail (df.weekStats,100)
+df.weekStats$channel
+
+### DEVELOPMENT
+#CASE ts65dn
+df.meanCase <- with (df.weekStats [which (df.weekStats$group == caseGroupLabel),] , aggregate (cbind (Number, Avg_Duration, Avg_Intake, Rate, Avg_Intermeal_Duration), list(channel=channel, group=group, period=period), mean))
+
+#CTRL
+df.meanControl <- with (df.weekStats [which (df.weekStats$group == controlGroupLabel),] , aggregate (cbind (Number, Avg_Duration, Avg_Intake, Rate, Avg_Intermeal_Duration), list(channel=channel, group=group, period=period), mean))
+
+#Formatting data frame with shape for heat map
+df.meanControl.m <- melt (df.meanControl, id.vars=c("channel", "group", "period"))
+df.meanCase.m <- melt (df.meanCase, id.vars=c("channel", "group", "period"))
+
+# Intermeal duration for water not calculated
+#
+df.meanCase.m$foldChange <- foldchange (df.meanCase.m$value,df.meanControl.m$value)
+
+length(df.meanCase.m$value)
+length(df.meanControl.m$value)
+df.meanCase.m$week <-df.meanCase.m$period
+df.meanCase.m$week <- paste ("week", df.meanCase.m$period, sep = "_")   
+
+#Ordering week column as period
+df.meanCase.m$period <- as.numeric (df.meanCase.m$period)
+df.meanCase.m$week <- with (df.meanCase.m, reorder (week, period,))
+
+#Merging channel and variable
+#df.meanCase.m$chVar <- paste (df.meanCase.m$channel, df.meanCase.m$variable, sep = "_")
+df.meanCase.m$variable <-  gsub ("_", " ", df.meanCase.m$variable, ignore.case = TRUE)
+
+#I want to insert this order Avg Intake, number, avg duration and rate, so the order is the same as in the other plots
+df.meanCase.m$varOrder [which (df.meanCase.m$variable == "Avg Intermeal Duration")] <-  "a"
+df.meanCase.m$varOrder [which (df.meanCase.m$variable == "Rate")] <-  "b"
+df.meanCase.m$varOrder [which (df.meanCase.m$variable == "Avg Duration")] <-  "c"
+df.meanCase.m$varOrder [which (df.meanCase.m$variable == "Number")] <-  "d"
+df.meanCase.m$varOrder [which (df.meanCase.m$variable == "Avg Intake")] <-  "e"
+
+df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Avg Intermeal Duration")] <-  "1"
+df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Rate")] <-  "2"
+df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Avg Duration")] <-  "3"
+df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Number")] <-  "4"
+df.meanCase.m$orderOut [which (df.meanCase.m$variable == "Avg Intake")] <-  "5"
 
 #Old command to order
 # df.meanCase.m <- df.meanCase.m [with (df.meanCase.m, order (period, channel, variable)),]
@@ -105,7 +195,14 @@ df.meanCase.m <- df.meanCase.m [with (df.meanCase.m, order (period, channel, ord
 # df.meanCase.m$variable [which (df.meanCase.m$variable == "Number" & df.meanCase.m$channel == "food_fat")] <-  "Meal Number"
 # df.meanCase.m$variable [which (df.meanCase.m$variable == "Number" & df.meanCase.m$channel == "water")] <-  "Drink Number"
 
-df.meanCase.m.Hab <- df.meanCase.m [df.meanCase.m$period == 1,]
+
+
+
+
+
+
+
+# df.meanCase.m.Hab <- df.meanCase.m [df.meanCase.m$period == 1,]
 
 #widthCol is used to specify for each column the percentage of the total width used
 #in this case only one column (habituation phase)
@@ -115,14 +212,9 @@ widthCol <- rep (0.09,length (unique (df.meanCase.m.Hab$week)))
 heatMapPlotterHab (df.meanCase.m.Hab, main="\n",  weekNotation="N", legPos="none",
                    xlab="\nHabituation Phase", ylab="Eating Behavior                          Drinking Behavior\n", widthCol=widthCol)
 
-#############
-#DEVELOPMENT
-#############
+ggsave("20140318_ts65_HF_heatMap_hab.pdf", width=10, height=8)
+# ggsave("20140318_ts65_HF_heatMap_dev.png", width=10, height=8, dpi=100)
 
-df.meanCase.m.Dev <- df.meanCase.m [df.meanCase.m$period > 1,]
-
-heatMapPlotter (df.meanCase.m.Dev, main="High-Fat Diet\n",  weekNotation="N", legPos="right",
-                xlab="\nDevelopment Phase (weeks)", ylab="Eating Behavior                          Drinking Behavior\n")
 
 #####################################
 ### Significance
@@ -196,7 +288,7 @@ heatMapPlotterHab (df.sigResults.Hab, main="\n",  weekNotation="N", legPos="none
 #########
 # Development
 df.sigResults.Dev <- df.sigResults [df.sigResults$period > 1, ]
-heatMapPlotter (df.sigResults.Dev, main="High-Fat diet\n",  weekNotation="N", legPos="right", mode="pvalues", 
+heatMapPlotter (df.sigResults.Dev, main="\n",  weekNotation="N", legPos="right", mode="pvalues", 
                 xlab="\nDevelopment Phase (weeks)", ylab="Food                                                  Water\n")
 
 
