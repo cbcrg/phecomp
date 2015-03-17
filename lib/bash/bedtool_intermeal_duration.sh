3172b4d5ab8b090c47586063acb2acd500610a8e
Jose Espinosa <kadomu@gmail.com>
2015-03-17 16:01:28 +0100

    Script gets the complement of meals and intersect them with the phases of the experiment using bedtools.

--- lib/bash/bedtool_intermeal_duration.sh
+++ lib/bash/bedtool_intermeal_duration.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 
-path2files="/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/"
+###########################################################################################
+###Jose Espinosa-Carrasco. CB/CSN-CRG. March 2015                                       ### 
+###########################################################################################
+### Using bedtools on meals converted to bed file by pergola generates the complement   ###
+### of these meals and intersect them with habituation and development                  ###
+###########################################################################################
+###                                                                                     ###
+###########################################################################################
+
+path2files="/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/intermeal_duration/data/"
 
 RScDir="/Users/jespinosa/git/phecomp/lib/R/"
 
-for track_HF in ${path2files}*food_fat*.bed
-do
+bed2intermeals () {
+	track=$1
+	
 	filename=$(basename "$track_HF")
 	filename="${filename%.*}"
+	
 	echo -e "Track of HF is ${filename}"
-	bedtools complement -i ${track_HF} -g ${path2files}all_mice.chromsizes | awk '{OFS="\t"; print $1,$2,$3,$3-$2}' > ${filename}.HF.compl
+	
+	return 0
+}
+
+for track in ${path2files}tr*.bed
+do
+	filename=$(basename "$track")
+	filename="${filename%.*}"
+	
+	bedtools complement -i ${track} -g ${path2files}all_mice.chromsizes | awk '{OFS="\t"; print $1,$2,$3,$3-$2}' > ${filename}.compl
+	echo -e "Track is ${track}"
+	awk '{OFS="\t"; print $1,$2,$3}' ${filename}.compl > ${filename}.compl.bed
+	
+	# Get the intermeal intervals of habituation
+	bedtools intersect -a ${track} -b ${path2files}exp_phases_hab.bed > ${filename}"_compl_hab.bed"
+	
+	# Get the intermeal intervals of development
+	bedtools intersect -a ${track} -b ${path2files}exp_phases_dev.bed > ${filename}"_compl_dev.bed"
 	
 done
-
-#for bed in ./*food_fat*.bed; do bedtools complement -i ${bed} -g /Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/coverageBamFiles/all_mice.chromsizes | awk '{OFS="\t"; #print $1,$2,$3,$3-$2}' > ${bed}.HF.compl; awk '{OFS="\t"; print $1,$2,$3}' ${bed}.HF.compl > ${bed}.HF.bed; done
