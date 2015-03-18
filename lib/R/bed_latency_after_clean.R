94085308714c35b925811e0fe1eb5fd9039a4073
Jose Espinosa <kadomu@gmail.com>
2015-03-18 17:47:35 +0100

    R script to analyze latency by track calculated using bedtools

--- lib/R/bed_latency_after_clean.R
+++ lib/R/bed_latency_after_clean.R
@@ -71,3 +71,30 @@ tbl_latency <- load_tbl_comp(pattern)
 head (tbl_latency)
 tail (tbl_latency)
 
+#Calculate mean and stderror of the mean
+# remove this row because is a whole file empty
+# chr1 996879 1007108 NA 1000  + 996879 1007108 chr1 1187875 1187911  NA 0.02   + 1187875 1187911 254,153,162 180768 10   HFlatency
+
+tbl_latency <- tbl_latency [!(tbl_latency$id ==10 & tbl_latency$V2 == 996879),]
+
+# I filter everything bigger than an hour 
+tbl_latency <- tbl_latency [which(tbl_latency$V18 < 3600),]
+tbl_latency
+tbl_stat_mean <-with (tbl_latency, aggregate (cbind (V18), list (group=group, index=index), FUN=function (x) c (mean=mean(x), std.error=std.error(x))))
+tbl_stat_mean
+
+tbl_stat_mean$mean <- tbl_stat_mean$V18 [,1]
+tbl_stat_mean$std.error <- tbl_stat_mean$V18 [,2]
+
+ggplot(data=tbl_stat_mean, aes(x=index, y=mean, fill=group)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=mean-std.error, ymax=mean+std.error),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) +
+  scale_x_continuous(breaks=1:9, limits=c(0,10))+
+  #        scale_y_continuous(limits=c(0,0.8))+
+  labs (title = "Latency First Meal\n") +  
+  labs (x = "\nFile Number\n", y="Latency (s)\n",fill = NULL) +
+  scale_fill_manual(values=cols, labels=c("Ctrl", "HF"))
+
+ggsave(file="bed_latency_after_clean.pdf",width=10, height=8)
