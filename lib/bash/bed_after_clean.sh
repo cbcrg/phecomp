f52c430bcb4acef211343ac7f6e024f169ea57ad
Jose Espinosa <kadomu@gmail.com>
2015-03-20 09:09:14 +0100

    Adding comment call

--- lib/bash/bed_after_clean.sh
+++ lib/bash/bed_after_clean.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Calling the script
+# ~/git/phecomp/lib/bash/bed_after_clean.sh
+
 path2files="/Users/jespinosa/phecomp/20140807_pergola/bedtools_ex/starting_regions_file_vs_24h/data/"
 
 RScDir="/Users/jespinosa/git/phecomp/lib/R/"
