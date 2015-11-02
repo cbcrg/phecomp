60e48a72977826d918b71a97008eecc845bc609a
Jose Espinosa <kadomu@gmail.com>
2015-11-02 12:07:18 +0100

    Adding circle plot of reinstatement variables

--- lib/R/pca_reinstatement.R
+++ lib/R/pca_reinstatement.R
@@ -12,6 +12,7 @@ library(Hmisc)
 library(calibrate)
 library(multcomp)
 library(ggplot2)
+library(FactoMineR)
 
 ##Getting HOME directory
 home <- Sys.getenv("HOME") 
@@ -85,8 +86,8 @@ home <- Sys.getenv("HOME")
 data_reinst <- read.csv (paste (home, "/old_data/data/Matrix 16_10_15 for CPA Reinstatement.csv", sep=""), dec=",", sep=";")
 head (data_reinst)
 
-data_reinst$X
-data_reinst_filt <- subset (data_reinst, select = -c (X))
+# data_reinst$X
+data_reinst_filt <- subset (data_reinst, select = -c(X))
 head (data_reinst_filt)
 length_tbl <- dim(data_reinst_filt) [2]
 data_reinst_filt$dep_active_day1
@@ -101,8 +102,7 @@ var_PC2 <- round (res$eig [2,2])
 pca2plot <- as.data.frame (res$ind$coord)
 pca2plot$id <- row.names(pca2plot)
 
-# pca2plot$days <-  as.factor(as.numeric (gsub(".*([0-9]+)$", "\\1", pca2plot$gen_day)))
-# pca2plot$gentreat <-  as.factor(gsub("([A-Z]+).*$", "\\1", pca2plot$gen_day))
+# Changes labels of the groups
 pca2plot$group <- data_reinst_filt$Group
 
 # Changing labels of the group
@@ -127,3 +127,46 @@ pca_medians_rev <- ggplot (pca2plot, aes(x=-Dim.1, y=-Dim.2, colour=group)) +
                            theme(legend.key=element_rect(fill=NA))
 
 pca_medians_rev
+
+###############
+### Circle Plot
+circle_plot <- as.data.frame (res$var$coord)
+labels_v <- row.names(res$var$coord)
+which (circle_plot$Dim.1 < 0)
+
+neg_labels <- labels_v [which (circle_plot$Dim.1 < 0)]
+neg_positions <- circle_plot [which (circle_plot$Dim.1 < 0), c(1,2)]
+
+# change positions for labels
+# neg_positions [2,2] <- neg_positions [2,2] - 0.03 
+# neg_positions [3,2] <- neg_positions [3,2] + 0
+# neg_positions [4,2] <- neg_positions [4,2] - 0.02
+
+pos_labels <- labels_v [which (circle_plot$Dim.1 >= 0)]
+pos_positions <- circle_plot [which (circle_plot$Dim.1 >= 0), c(1,2)]
+
+angle <- seq(-pi, pi, length = 50)
+df.circle <- data.frame(x = sin(angle), y = cos(angle))
+
+#aes(x=PC1, y=PC2, colour=gentreat )) 
+p_circle_plot <- ggplot(circle_plot) + 
+                 geom_segment (data=circle_plot, aes(x=0, y=0, xend=Dim.1, yend=Dim.2), arrow=arrow(length=unit(0.2,"cm")), alpha=1, size=1, color="red") +
+                 xlim (c(-1.2, 1.2)) + ylim (c(-1.2, 1.2)) +
+#                  geom_text (data=circle_plot, aes (x=Dim.1, y=Dim.2, label=labels_v, hjust=1.2), show_guide = FALSE, size=5) + 
+                 geom_text (data=neg_positions, aes (x=Dim.1, y=Dim.2, label=neg_labels, hjust=1.2), show_guide = FALSE, size=5) + 
+                 geom_text (data=pos_positions, aes (x=Dim.1, y=Dim.2, label=pos_labels, hjust=-0.3), show_guide = FALSE, size=5) +
+                 geom_vline (xintercept = 0, linetype="dotted") +
+                 geom_hline (yintercept=0, linetype="dotted") +
+                 labs (title = "PCA of the variables\n", x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
+                       y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
+                 #        geom_polygon(aes(x, y), data = df, inherit.aes = F, Fill=NA)
+                 #                         scale_x_continuous(breaks=1:10)  
+                 geom_polygon (data = df.circle, aes(x, y), alpha=1, colour="black", fill=NA, size=1)
+
+base_size <- 10
+p_circle_plot
+dailyInt_theme <- theme_update (axis.title.x = element_text (size=base_size * 2, face="bold"),
+                                axis.title.y = element_text (size=base_size * 2, angle = 90, face="bold"),
+                                plot.title = element_text (size=base_size * 2, face="bold"))
+
+p_circle_plot
